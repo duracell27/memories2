c01f76d179e6d7a2b18ae41145e731c0c1f3ada5
duracell27 <volodymyrshmidt@gmail.com>
2021-10-11 18:27:56 +0300

    google auth half

--- client/src/Auth/Auth.js
+++ client/src/Auth/Auth.js
@@ -1,63 +1,146 @@
-import React, { useState } from 'react'
-import useStyles from './styles'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
-import { Avatar, Button, Container, Grid, Paper, TextField, Typography } from '@material-ui/core'
-import Input from './Input'
+import React, { useState } from "react";
+import useStyles from "./styles";
+import { GoogleLogin } from "react-google-login";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Icon from './icon'
+import {useDispatch} from 'react-redux'
+import {
+  Avatar,
+  Button,
+  Container,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import Input from "./Input";
 
 export default function Auth() {
-    const classes = useStyles()
-    const [isSignup, setIsSignup] = useState(true)
-    //const isSignup = true
-    const [showPassword, setShowPassword] = useState(false)
+  const classes = useStyles();
+ const dispatch = useDispatch()
+  const [isSignup, setIsSignup] = useState(true);
 
-    const handleShowPassword = () => {
-        setShowPassword((prevShowPassword) => !prevShowPassword)
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleShowPassword = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  const handleSubmit = () => {};
+
+  const handleChange = () => {};
+
+  const switchMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+    handleShowPassword(false);
+  };
+
+  const googleSuccess = async (res) => {
+    const result = res?.profileObj
+    const token = res?.tokenId
+
+    try {
+        dispatch({type: 'AUTH', data: {result, token}})
+    } catch (error) {
+        console.log(error)
     }
+  }
 
-    const handleSubmit = () => {
-
-    }
-
-    const handleChange = () => {
-
-    }
-
-    const switchMode = () => {
-        setIsSignup((prevIsSignup) => !prevIsSignup)
-        handleShowPassword(false)
-    }
-    return (
-        <Container component='main' maxWidth='xs'>
-            <Paper className={classes.paper} elevation={3}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography variant='h5'>{isSignup ? 'Зареєструватися' : 'Увійти'}</Typography>
-                <form onSubmit={handleSubmit} className={classes.form}>
-                    <Grid container spacing={2}>
-                        {
-                            isSignup && (
-                                <>
-                                    <Input name='firstName' label="Імя" handleChange={handleChange} autoFocus half />
-                                    <Input name='lastName' label="Прізвище" handleChange={handleChange} half />
-                                </>
-                            )
-                        }
-                        <Input name='email' label="Email" handleChange={handleChange} type='email' />
-                        <Input name='password' label="Пароль" handleChange={handleChange} type='password' />
-                        {isSignup ? (<Input name='confirmPassword' label="Підтвердження пароля" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />) : null}
-
-                    </Grid>
-                    <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>{isSignup ? 'Зареєструватися' : 'Увійти'}</Button>
-                    <Grid container justify='center'>
-                        <Grid item>
-                            <Button  variant='contained' color='secondary' onClick={switchMode}>
-                                {isSignup ? 'Маєте акаунт? Увійдіть' : 'Не маєте акаунта? Зареєструйтеся'}
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </form>
-            </Paper>
-        </Container>
-    )
+  const googleFailure = () => {
+      console.log('Спроба вхоу була невдала. Спробуйте ще раз')
+  }
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">
+          {isSignup ? "Зареєструватися" : "Увійти"}
+        </Typography>
+        <form onSubmit={handleSubmit} className={classes.form}>
+          <Grid container spacing={2}>
+            {isSignup && (
+              <>
+                <Input
+                  name="firstName"
+                  label="Імя"
+                  handleChange={handleChange}
+                  autoFocus
+                  half
+                />
+                <Input
+                  name="lastName"
+                  label="Прізвище"
+                  handleChange={handleChange}
+                  half
+                />
+              </>
+            )}
+            <Input
+              name="email"
+              label="Email"
+              handleChange={handleChange}
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Пароль"
+              handleChange={handleChange}
+              type="password"
+            />
+            {isSignup ? (
+              <Input
+                name="confirmPassword"
+                label="Підтвердження пароля"
+                handleChange={handleChange}
+                type={showPassword ? "text" : "password"}
+                handleShowPassword={handleShowPassword}
+              />
+            ) : null}
+          </Grid>
+          
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            {isSignup ? "Зареєструватися" : "Увійти"}
+          </Button>
+          <GoogleLogin
+            clientId="454038135077-ufneqpogkj2996g6d9ujbjurq4ac75g1.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <Button
+                className={classes.googleButton}
+                color="primary"
+                fullWidth
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                variant="contained"
+                startIcon={<Icon />}
+              >Увіти через Google</Button>
+            )}
+                onSuccess={googleSuccess}
+          onFailure={googleFailure}
+          cookiePolicy='single_host_origin'
+          />
+          <Grid container justifyContent="center">
+            <Grid item>
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={switchMode}
+              >
+                {isSignup
+                  ? "Маєте акаунт? Увійдіть"
+                  : "Не маєте акаунта? Зареєструйтеся"}
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
 }
