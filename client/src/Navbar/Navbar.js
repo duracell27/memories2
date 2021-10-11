9cd036178d1ae3d46c890d05aa88a54b11457632
Duracell27 <volodymyrshmidt@gmail.com>
2021-10-11 23:08:38 +0300

    google auth is ready

--- client/src/Navbar/Navbar.js
+++ client/src/Navbar/Navbar.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import { AppBar, Avatar, Button, Toolbar, Typography } from "@material-ui/core";
 import memories from "./../images/memories.png";
 import useStyles from "./styles"
+import { useDispatch } from 'react-redux';
+import { useHistory, useLocation } from "react-router";
 
 export default function Navbar() {
     const classes = useStyles()
-
-    const user = null
+    const history = useHistory()
+    const location = useLocation()
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')))
+    const dispatch = useDispatch()
+    const logout = ()=>{
+        dispatch({type: 'LOGOUT'})
+        history.push('/')
+        setUser(null)
+    }
+    
+    useEffect(()=>{
+        const token = user?.token
+        setUser(JSON.parse(localStorage.getItem('profile')))
+    },[location])
 
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
@@ -21,7 +35,7 @@ export default function Navbar() {
                 {user ? (<div className={classes.profile}>
                     <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
                     <Typography className={classes.userName} variant='h6'>{user.result.name}</Typography>
-                    <Button variant='contained' color='secondary' className={classes.logout}>вийти</Button>
+                    <Button variant='contained' color='secondary' className={classes.logout} onClick={logout}>вийти</Button>
                 </div>) : (<Button component={Link} to='/auth' variant='contained' color='primary'>Увійти</Button>)}
             </Toolbar>
         </AppBar>
