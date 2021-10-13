1493b11fc79dde9d1bd5710e0bbde084cafed5af
duracell27 <volodymyrshmidt@gmail.com>
2021-10-13 18:36:38 +0300

    some provements

--- client/src/Form/Form.js
+++ client/src/Form/Form.js
@@ -10,13 +10,13 @@ export default function Form({ currentId, setCurrentId }) {
     const classes = useStyles()
     console.log(currentId)
     const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId):null)
-
+    const user = JSON.parse(localStorage.getItem('profile'))
     useEffect(()=>{
         if(post) setPostData(post)
     },[post])
 
     const [postData, setPostData] = useState({
-        creator: '', title: '', message: '', tags: '', selectedFile: ''
+         title: '', message: '', tags: '', selectedFile: ''
     })
     const dispatch = useDispatch()
 
@@ -24,9 +24,9 @@ export default function Form({ currentId, setCurrentId }) {
         e.preventDefault()
 
         if (currentId) {
-            dispatch(updatePost(currentId, postData))
+            dispatch(updatePost(currentId, {...postData, name: user?.result?.name})) 
         } else {
-            dispatch(createPost(postData))
+            dispatch(createPost({...postData, name: user?.result?.name}))
         }
         clear()
 
@@ -35,18 +35,29 @@ export default function Form({ currentId, setCurrentId }) {
     const clear = () => {
         setCurrentId(null)
         setPostData({
-            creator: '', title: '', message: '', tags: '', selectedFile: ''
+             title: '', message: '', tags: '', selectedFile: ''
         })
     }
+
+    if(!user?.result?.name){
+        return (
+            <Paper className={classes.paper}>
+                <Typography variant='h6' align='center'>
+                    Зареєструйтесь будь ласка, щоб створити згадку, чи лайкати згадки)
+                </Typography>
+            </Paper>
+        )
+    }
+
     return (
         <div>
             <Paper className={classes.paper}>
                 <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
                     <Typography variant='h6'>{currentId? 'Редагувати' : 'Створити'} згадку</Typography>
-                    <TextField name='creator' variant='outlined' label='Автор' fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
+                    
                     <TextField name='title' variant='outlined' label='Заголовок' fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
                     <TextField name='message' variant='outlined' label='Текст' fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
-                    <TextField name='tags' variant='outlined' label='Теги' fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.replace(/\s+/g, '').split(',') })} />
+                    <TextField name='tags' variant='outlined' label='Теги (розділені комою)' fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.replace(/\s+/g, '').split(',') })} />
                     <div className={classes.fileInput}>
                         <FileBase
                             type='file' multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })}
